975ef0015d1658ef46de00cdf85d131e51bfe37d
j- <jh@live.com.au>
2015-01-28 15:03:00 +1100

    Implemented array pauser functionality

--- addon/array-pauser.js
+++ addon/array-pauser.js
@@ -1,7 +1,45 @@
 import Em from 'ember';
+var get = Em.get;
+var copy = Em.copy;
 
 var ArrayPauser = Em.ArrayProxy.extend({
-	isPaused: false
+	isPaused: false,
+
+	buffer: Em.A(),
+
+	addToBuffer: function (idx, removedCount, added) {
+		var buffer = get(this, 'buffer');
+		buffer.pushObject([idx, removedCount, added]);
+	},
+
+	clearBuffer: function () {
+		var buffer = get(this, 'buffer');
+		var arrangedContent = get(this, 'arrangedContent');
+		buffer.forEach(function (args) {
+			arrangedContent.replace(args[0], args[1], args[2]);
+		});
+		buffer.clear();
+	}.observes('isPaused'),
+
+	arrangedContent: function () {
+		var content = get(this, 'content');
+		var clone = copy(content);
+		return clone;
+	}.property('content'),
+
+	contentArrayDidChange: function (arr, idx, removedCount, addedCount) {
+		var added = arr.slice(idx, idx + addedCount);
+		var isPaused = get(this, 'isPaused');
+		var arrangedContent;
+		if (isPaused) {
+			this.addToBuffer(idx, removedCount, added);
+		}
+		else {
+			arrangedContent = get(this, 'arrangedContent');
+			arrangedContent.replace(idx, removedCount, added);
+		}
+		this.enumerableContentDidChange();
+	}
 });
 
 export default ArrayPauser;
