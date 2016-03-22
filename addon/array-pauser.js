4cba493d24ecbc5cbc97ca44678c88b831fc8a25
j- <j@skeoh.com>
2016-03-22 15:14:18 +1100

    Upgrade Ember to 1.13.13

--- addon/array-pauser.js
+++ addon/array-pauser.js
@@ -1,33 +1,34 @@
 import Em from 'ember';
+import { ArrayController } from 'ember-legacy-controllers';
 var get = Em.get;
 var copy = Em.copy;
 
-var ArrayPauser = Em.ArrayController.extend({
+var ArrayPauser = ArrayController.extend({
 	isPaused: false,
 
-	buffer: function () {
+	buffer: Em.computed(function () {
 		return Em.A();
-	}.property(),
+	}),
 
 	addToBuffer: function (idx, removedCount, added) {
 		var buffer = get(this, 'buffer');
 		buffer.pushObject([idx, removedCount, added]);
 	},
 
-	clearBuffer: function () {
+	clearBuffer: Em.observer('isPaused', function () {
 		var buffer = get(this, 'buffer');
 		var arrangedContent = get(this, 'arrangedContent');
-		buffer.forEach(function (args) {
-			arrangedContent.replace(args[0], args[1], args[2]);
+		buffer.forEach(function ([idx, removedCount, added]) {
+			arrangedContent.replace(idx, removedCount, added);
 		});
 		buffer.clear();
-	}.observes('isPaused'),
+	}),
 
-	arrangedContent: function () {
+	arrangedContent: Em.computed('content', function () {
 		var content = get(this, 'content');
 		var clone = copy(content);
 		return clone;
-	}.property('content'),
+	}),
 
 	contentArrayDidChange: function (arr, idx, removedCount, addedCount) {
 		var added = arr.slice(idx, idx + addedCount);
